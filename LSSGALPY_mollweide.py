2e9be3625e87951bd54ecbe5b87ab28615eee5bc
Maria Argudo Fernandez <mamen.argudo@gmail.com>
2015-02-17 17:05:25 +0800

    Update LSSGALPY_mollweide.py

--- LSSGALPY_mollweide.py
+++ LSSGALPY_mollweide.py
@@ -14,35 +14,32 @@ from matplotlib.widgets import Slider, Button, CheckButtons
 [ra, dec, z], [ra_isol, dec_isol, z_isol], [ra_pair, dec_pair, z_pair], [ra_trip, dec_trip, z_trip] = [np.loadtxt(filename+'.txt', usecols = (0, 1, 2), unpack=True) for filename in ['SDSS_DR10_galaxy_local', 'table1', 'table2', 'table3']]
 
 ra_tot, dec_tot, z_tot = [ra, ra_isol, ra_pair, ra_trip], [dec, dec_isol, dec_pair, dec_trip], [z, z_isol, z_pair, z_trip]
+cond_z_tot = lambda zi, rangi: [((zval > zi) & (zval < zi + rangi)) for zval in z_tot] # Condition for the redshift range in the plots
 
 # Default values for reset button
 z0, rang0, dec0, alpha0 = .030, .005, 30., .2
 
-# Plot
+# Main plot: Mollweide projection
 fig = plt.figure("Mollweide projection", figsize=(16.3, 8.4))
 plt.subplot(111, xticks=[], yticks=[], frameon=False)
 ax2, ax1 = plt.axes([0.79, 0.05, 0.25, 0.25], polar=True), fig.add_axes([0.05, 0.14, 0.9, 0.8])
 
-# Mollweide projection
 m = Basemap(projection='moll', lon_0=180, resolution='c', celestial=True, ax=ax1)
-
-# Main plot: Mollweide projection
 m.drawmeridians(np.arange(0., 420., 60.)) ; m.drawparallels(np.arange(-90., 120., 30.), labels=[1, 0, 0, 1]) ; m.drawmapboundary(fill_color='w')
+x, y = [[m(raval, decval)[i] for raval, decval in zip(ra_tot, dec_tot)] for i in range(0, 2)] # Translation of R.A. and Dec. positions to Mollweide projection
 
-x, y = [[m(raval, decval)[i] for raval, decval in zip(ra_tot, dec_tot)] for i in range(0, 2)] # Translation of RA and DEC positions to Mollweide proj
-cond_z_tot = [((zval > z0) & (zval < z0+rang0)) for zval in z_tot] # Condition for the redhift range in the plots
-
-# Plotting galaxy positions in the selected redhift range
-xyplt = [plt.plot(x[i][cond_z_tot[i]], y[i][cond_z_tot[i]], krgb, ms=mval, alpha=alpval, visible=visi)[0] for i, krgb, mval, alpval, visi in zip(range(0, 4), ['ko', 'ro', 'go', 'bo'], [1, 4, 4, 4], [alpha0, .7, .7, .7], [True, True, False, False])]
+# Plotting galaxy positions in the selected redshift range
+xyplt = [plt.plot(x[i][cond_z_tot(z0, rang0)[i]], y[i][cond_z_tot(z0, rang0)[i]], krgb, ms=mval, alpha=alpval, visible=visi)[0] for i, krgb, mval, alpval, visi in zip(range(0, 4), ['ko', 'ro', 'go', 'bo'], [1, 4, 4, 4], [alpha0, .7, .7, .7], [True, True, False, False])]
 
 # Additional plot: Wedge diagram
-cond_dec = (dec > -1.) & (dec <= 1.)
-cond_z = (z[cond_dec] >= z0) & (z[cond_dec] < z0 + rang0)
-
-[ax2.scatter(np.radians(raval - 95), zval, c=colors, s=1, marker=',', lw=0, alpha=alpval) for raval, zval, colors, alpval in zip([ra[cond_dec], ra[cond_dec][cond_z]], [z[cond_dec], z[cond_dec][cond_z]], [z[cond_dec], 'r'], [.1, 1])]
-ax2.bar(0., rang0, width=2*np.pi, bottom=z0, color='r', edgecolor='r', lw=0, alpha=.4)
-ax2.set_rmax(.1)
-[plt.setp(getval, fontsize=fontval) for getval, fontval in zip([ax2.get_xticklabels(), ax2.get_yticklabels()], [10, 8])]
+def wedgefig(zini, rangi):
+    cond_dec = (dec > -1.) & (dec <= 1.)
+    cond_z = (z[cond_dec] >= zini) & (z[cond_dec] < zini + rangi)
+    [ax2.scatter(np.radians(raval - 95), zval, c=colors, s=1, marker=',', lw=0, alpha=alpval) for raval, zval, colors, alpval in zip([ra[cond_dec], ra[cond_dec][cond_z]], [z[cond_dec], z[cond_dec][cond_z]], [z[cond_dec], 'r'], [.1, 1])]
+    ax2.bar(0., rangi, width=2*np.pi, bottom=zini, color='r', edgecolor='r', lw=0, alpha=.4)
+    ax2.set_rmax(.1)
+    [plt.setp(getval, fontsize=fontval) for getval, fontval in zip([ax2.get_xticklabels(), ax2.get_yticklabels()], [10, 8])]
+wedgefig(z0, rang0)
 
 # Location of sliders in the figure
 axz, axrang, axalpha = [plt.axes(val, axisbg='lightgoldenrodyellow') for val in [[0.25, 0.06, 0.5, 0.03], [0.25, 0.02, 0.5, 0.03], [0.05, 0.1, 0.07, 0.03]]]
@@ -57,17 +54,12 @@ fig_text = plt.figtext(0.5, 0.11, 'Mollweide projection within %1.3f < z < %1.3f
 def update(val):
     '''This function updates the main and additional plots for new values of redshift, redshift range, and points transparency'''
     rang, zCen, newAlpha = [sval.val for sval in [srang, sz, salpha]]
-    cond_df = [((zval >= zCen) & (zval < zCen+rang)) for zval in z_tot]
-    cond_z = (z[cond_dec] >= zCen) & (z[cond_dec] < zCen+rang)
     # Update the value of the Text object
     fig_text.set_text('Mollweide projection within %1.3f < z < %1.3f' % (round(zCen,3), round(zCen,3)+round(rang,3)))
     ax2.cla()
-    [ax2.scatter(np.radians(raval - 95), zval, c=colors, s=1, marker=',', lw=0, alpha=alpval) for raval, zval, colors, alpval in zip([ra[cond_dec], ra[cond_dec][cond_z]], [z[cond_dec], z[cond_dec][cond_z]], [z[cond_dec], 'r'], [.1, 1])]
-    [xyp.set_data(x[i][cond_df[i]], y[i][cond_df[i]]) for xyp, i in zip(xyplt, range(0, 4))]
-    [plt.setp(getval, fontsize=fontval) for getval, fontval in zip([ax2.get_xticklabels(), ax2.get_yticklabels()], [10, 8])]
-    ax2.bar(0., rang, width=2*np.pi, bottom=zCen, color='r', edgecolor='r', lw=0, alpha=.4)
-    ax2.set_rmax(.1)
-    xyplt[0].set_alpha(alpha=newAlpha)  
+    [xyp.set_data(x[i][cond_z_tot(zCen, rang)[i]], y[i][cond_z_tot(zCen, rang)[i]]) for xyp, i in zip(xyplt, range(0, 4))]
+    xyplt[0].set_alpha(alpha=newAlpha)
+    wedgefig(zCen, rang)
     plt.draw()
 [sval.on_changed(update) for sval in [sz, srang, salpha]]
 
